75b78b295c999a849199c298863603b435cb7712
sdiazben <sara.diaz@upc.edu>
2019-03-28 15:48:37 +0100

    added env

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/saradiaz/Documents/UPC/CC/lab4/eb-django-express-signup-base/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
